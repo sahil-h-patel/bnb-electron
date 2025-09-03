b6ea2160d5d1c1bf1e0365fc7d0e985ccba825bc
sahil <shp2382@g.rit.edu>
2025-09-02 21:29:02 -0400

    fixed git stuff

--- src/renderer/src/lib/trpc.ts
+++ src/renderer/src/lib/trpc.ts
@@ -1,6 +1,6 @@
 import { createTRPCProxyClient } from '@trpc/client'
 import { ipcLink } from 'electron-trpc/renderer'
-import { AppRouter } from '@main/api'
+import { AppRouter } from '../../../main/api'
 
 export const client = createTRPCProxyClient<AppRouter>({
   links: [ipcLink()]
